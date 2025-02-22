52f832371e83db77af1ed27b33be18e7e2b40600
Benzazoua <billalben252@gmail.com>
2025-02-22 21:43:45 +0100

    Menu - About window

--- main.js
+++ main.js
@@ -1,40 +1,101 @@
 const path = require('path');
-const { app, BrowserWindow } = require('electron/main');
+const { app, BrowserWindow, Menu } = require('electron/main');
 
+const isMac = process.platform === 'darwin';
+const isDev = process.env.NODE_ENV !== 'production';
+
+let mainWindow;
+
+// Main window
 const createWindow = () => {
-  const isDev = process.env.NODE_ENV !== 'production';
-
-  const win = new BrowserWindow({
-    title: 'Image Ressizer',
-    width: isDev ? 1200 : 800,
+  mainWindow = new BrowserWindow({
+    title: 'Image Resizer',
+    width: isDev ? 1100 : 800,
     height: 600,
+    icon: path.resolve(__dirname, 'render/images/Icon_256x256.png'),
+    resizable: isDev,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true,
+    },
   });
 
-  // Show devtools automatically if in development
   if (isDev) {
-    win.webContents.openDevTools();
+    mainWindow.webContents.openDevTools();
   }
 
-  // win.loadFile('./render/index.html')
-  win.loadFile(path.join(__dirname, './render/index.html'));
+  mainWindow.loadFile(path.resolve(__dirname, 'render/index.html'));
 };
 
+// About Window
+const createAboutWindow = () => {
+  const aboutWin = new BrowserWindow({
+    width: 300,
+    height: 300,
+    title: 'About Electron',
+    icon: path.resolve(__dirname, 'render/images/Icon_256x256.png'),
+    resizable: false,
+  });
+
+  aboutWin.loadFile(path.resolve(__dirname, 'render/about.html'));
+
+  // Hide menu in the About window
+  aboutWin.setMenu(null);
+};
+
+const menuTemplate = [
+  ...(isMac ? [{ label: app.name, submenu: [{ label: 'About', click: createAboutWindow }] }] : []),
+  { role: 'fileMenu' },
+  ...(!isMac ? [{ label: 'Help', submenu: [{ label: 'About', click: createAboutWindow }] }] : []),
+  ...(isDev
+    ? [
+        {
+          label: 'Developer',
+          submenu: [{ role: 'reload' }, { role: 'forcereload' }, { type: 'separator' }, { role: 'toggledevtools' }],
+        },
+      ]
+    : []),
+];
+
 app.whenReady().then(() => {
-  // Ensures Electron is initialized before opening a window
   createWindow();
 
-  // Handles macOS behavior, reopening a window when clicking the app icon
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+
+  // Remove variable from memory
+  // mainWindow.on('closed', () => (mainWindow = null));
+  mainWindow.on('close', (event) => {
+    if (isMac === 'darwin') {
+      event.preventDefault(); // Keep the app running on macOS
+      mainWindow.hide();
+    } else {
+      mainWindow = null;
+    }
+  });
+
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
   });
+
+  // Close the app completely on Windows/Linux, keep running on macOS
+  app.on('window-all-closed', () => {
+    if (!isMac) {
+      app.quit();
+    }
+  });
 });
 
-// Closes the app completely on Windows/Linux, but keeps it running on macOS
-app.on('window-all-closed', () => {
-  // darwin for macos - wind32 for windows
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+const gotTheLock = app.requestSingleInstanceLock();
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on('second-instance', () => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.focus();
+    }
+  });
+}
